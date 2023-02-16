d7b793c9bbf20a5a172d8bd2d0f372f8d862d4d9
Zack <zackrylangford@gmail.com>
2023-02-15 21:57:40 -0500

    Fixed API variable and began working on database connection

--- august/august.py
+++ august/august.py
@@ -1,8 +1,10 @@
+import os
+#import psycopg2
 import openai
 from functions import append_user_input, append_august_input
 
 # OpenAI settings
-openai.api_key = "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"
+openai.api_key = os.environ["OPENAI_API_KEY"]
 model_engine = "text-davinci-003"
 
 
@@ -11,7 +13,13 @@ username = "Zackry"
 ai_username = "August"
 
 # Database Settings
-
+#conn = psycopg2.connect(
+#    database= "chatgptdb",
+ #   user="august",
+ #   password="robot",
+#    host="localhost",
+ #   port="5432"
+ #   )
 
 
 
@@ -20,9 +28,7 @@ prompt = input(f"\n{username}: ")
 
 # Conversation Loop
 while prompt != '--quit':
-    
-    # Add user input to database
-    append_user_input("user-data.txt", prompt)
+
 
     if prompt != '-quit':
         completion = openai.Completion.create(
@@ -46,8 +52,11 @@ while prompt != '--quit':
     response = completion.choices[0].text
     print(f"\n{ai_username}: {response.strip()}")
     
+    # Add user input to database
+    append_user_input("/home/zack/Desktop/GitHub/Private/august-gpt/august/user-logs.txt", prompt)
+
     #Add Marcus output to database
-    append_august_input("august-data.txt", response.strip())
+    append_august_input("/home/zack/Desktop/GitHub/Private/august-gpt/august/august-logs.txt", response.strip())
 
     prompt = input(f"\n{username}: ")
 
